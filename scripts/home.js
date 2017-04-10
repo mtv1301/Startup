c101eacc908b67d24fec9ac26968a911939592dc
mtv1301 <mtv1301@gmail.com>
2017-04-10 22:33:22 +0300

    start working with second section

--- scripts/home.js
+++ scripts/home.js
@@ -1,3 +1,28 @@
 /**
  * Created by Tetiana on 05.03.2017.
  */
+$(document).ready(function () {
+
+    $(".cross").hide();
+
+    $(".hamburger-menu").click(function () {
+        console.log('menu display: show');
+        $(".style-menu-small").slideToggle("slow", function () {
+            $(".hamburger-menu").hide();
+            $(".cross").show();
+        });
+    });
+    $(".cross").click(function () {
+        console.log('menu display: none');
+        $(".style-menu-small").slideToggle("slow", function () {
+            $(".cross").hide();
+            $(".hamburger-menu").show();
+        });
+    });
+    $(".hamburger-menu").click(function () {
+        $("header").addClass( "blur" );
+    });
+    $(".cross").click(function () {
+        $( "header" ).removeClass( "blur" );
+    });
+});
